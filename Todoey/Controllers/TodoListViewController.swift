3368ce151f504303359608543e9067e6c8a3faab
Sergio Manrique <sergiman94@gmail.com>
2018-03-20 21:25:44 -0500

    convert to MVC

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -11,16 +11,22 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     
-    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // retroalimentamos la informacion agregada recientemente para que no se borre al momento de terminar la app
-        if let items = defaults.array(forKey: "TodoListArray") as?  [String] {
-            
+        
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        newItem.done = true
+        itemArray.append(newItem)
+        
+//        // retroalimentamos la informacion agregada recientemente para que no se borre al momento de terminar la app
+        if let items = defaults.array(forKey: "TodoListArray") as?  [Item] {
+
             itemArray = items
         }
         
@@ -37,11 +43,29 @@ class TodoListViewController: UITableViewController {
     // funcion que permite gestionar con cada celda
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        // seleccion de cada celda con el identificador
+        
+        // seleccion de cada celda con el identificador (se estan reutilizando las celdas)
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
+        // cada item de la lista
+        let item = itemArray[indexPath.row]
+        
         // texto que va en cada celda - arreglo
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        
+        // ternary operator ==>
+        // value = condition ? valueIfTrue : calueIfFalse
+        
+        // representacion de la estructura condicional de abajo con el operador ternario
+        
+        cell.accessoryType = item.done == true ? .checkmark : .none
+        
+//        if item.done == true {
+//            cell.accessoryType = .checkmark
+//        }else{
+//            cell.accessoryType = .none
+//        }
         
         return cell
         
@@ -49,20 +73,28 @@ class TodoListViewController: UITableViewController {
     
     // funcion que detecta cuando se ah seleccionado una fila - cuando se ha seleccionado una fila
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
         // imprimimos en consola la fila que hemos selccionado
         print(itemArray[indexPath.row])
+        
+        
+        tableView.reloadData()
+        
         
         // no dejamos seleccionada cada fila que oprimimos
         tableView.deselectRow(at: indexPath, animated: true)
         
-         // si ya marcamos con un accesorio (chulo) la fila que hemos seleccionado
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            // no marque otra vez, quitala
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            // marca
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+//         // si ya marcamos con un accesorio (chulo) la fila que hemos seleccionado
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+//            // no marque otra vez, quitala
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        }else{
+//            // marca
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
         
         
     }
@@ -79,8 +111,13 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen once the user clicks the add item button on our alert
             
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            
             // insertamos un nuevo item al arreglo con la informacion del textField
-            self.itemArray.append(textField.text!)
+            self.itemArray.append(newItem)
             
             // guardamos las actualizaciones hechas en la aplicacion para que no se borren al momento de terminarla 
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Sergio Manrique on 3/20/18.
+//  Copyright © 2018 Clever.Inc. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
